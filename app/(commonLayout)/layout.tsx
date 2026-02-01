2a83b558bccb0ad2472aecc28edc2bd08457f074
sajidalhafiz-dev <sajidalhafiz.dev@gmail.com>
2026-02-01 15:36:08 +0600

    project init with common & dashboard layout

---
+++ app/(commonLayout)/layout.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "@/components/shared/footer";
+import { Navbar } from "@/components/shared/Navbar";
+
+export default function commonLayout ({children}: {children:React.ReactNode}) {
+
+    return (
+        <div>
+            <Navbar/>
+            {children}
+            <Footer/>
+        </div>
+    );
+}
---
+++ app/(commonLayout)/page.tsx
@@ -0,0 +1,14 @@
+import { ComponentExample } from "@/components/component-example";
+import { Button } from "@/components/ui/button";
+
+// export default function Page() {
+//   return <ComponentExample />;
+// }
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
